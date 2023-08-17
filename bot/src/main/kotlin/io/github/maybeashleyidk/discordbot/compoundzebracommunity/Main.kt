80f194a62472615e5da7bbcb4327f44d310cd123
MaybeAshleyIdk <142437333+MaybeAshleyIdk@users.noreply.github.com>
2023-08-17 19:49:40 +0200

    Implement the main function

--- bot/src/main/kotlin/io/github/maybeashleyidk/discordbot/compoundzebracommunity/Main.kt
+++ bot/src/main/kotlin/io/github/maybeashleyidk/discordbot/compoundzebracommunity/Main.kt
@@ -2,6 +2,30 @@
 
 package io.github.maybeashleyidk.discordbot.compoundzebracommunity
 
+import javax.annotation.CheckReturnValue
+import kotlin.system.exitProcess
+
 fun main() {
-	// TODO
+	val token: BotToken = getBotTokenFromEnvironmentOrExit()
+	Bot.run(token = token)
+}
+
+@CheckReturnValue
+private fun getBotTokenFromEnvironmentOrExit(): BotToken {
+	return when (val environmentBotToken: EnvironmentBotToken = Environment.extractBotToken()) {
+		is EnvironmentBotToken.UnsetOrEmpty -> {
+			System.err.println("environment variable ${Environment.BOT_TOKEN_VARIABLE_NAME} must not be unset or empty")
+			exitProcess(48)
+		}
+
+		is EnvironmentBotToken.InvalidLength -> {
+			val msg: String = "value of environment variable ${Environment.BOT_TOKEN_VARIABLE_NAME}" +
+				" must be exactly ${BotToken.TOKEN_STRING_LENGTH} characters long"
+			System.err.println(msg)
+
+			exitProcess(49)
+		}
+
+		is EnvironmentBotToken.Valid -> environmentBotToken.token
+	}
 }
