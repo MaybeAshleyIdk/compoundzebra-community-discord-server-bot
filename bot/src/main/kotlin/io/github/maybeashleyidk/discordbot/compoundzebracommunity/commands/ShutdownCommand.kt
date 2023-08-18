b3c8f72bac61f9c0a878d9499de4b3f01582696e
MaybeAshleyIdk <142437333+MaybeAshleyIdk@users.noreply.github.com>
2023-08-18 11:51:57 +0200

    Extract strings into config

--- bot/src/main/kotlin/io/github/maybeashleyidk/discordbot/compoundzebracommunity/commands/ShutdownCommand.kt
+++ bot/src/main/kotlin/io/github/maybeashleyidk/discordbot/compoundzebracommunity/commands/ShutdownCommand.kt
@@ -1,6 +1,8 @@
 package io.github.maybeashleyidk.discordbot.compoundzebracommunity.commands
 
 import io.github.maybeashleyidk.discordbot.compoundzebracommunity.ShutdownManager
+import io.github.maybeashleyidk.discordbot.compoundzebracommunity.config.Config
+import io.github.maybeashleyidk.discordbot.compoundzebracommunity.config.ConfigLoader
 import io.github.maybeashleyidk.discordbot.compoundzebracommunity.logging.Logger
 import net.dv8tion.jda.api.Permission
 import net.dv8tion.jda.api.entities.Member
@@ -10,30 +12,32 @@ import javax.annotation.CheckReturnValue
 import javax.inject.Inject
 
 class ShutdownCommand @Suppress("ktlint:standard:annotation") @Inject constructor(
+	private val configLoader: ConfigLoader,
 	private val shutdownManager: ShutdownManager,
 	private val logger: Logger,
 ) : Command(name = CommandName.ofString("shutdown")) {
 
 	override fun execute(catalystMessage: Message, textChannel: TextChannel) {
 		val authorAsGuildMember: Member? = catalystMessage.getAuthorAsGuildMember()
+		val config: Config = this.configLoader.load()
 
 		if (authorAsGuildMember == null) {
 			this.logger.logWarning("Guild.getMemberById() returned null for some reason. Bailing out")
 
-			textChannel.sendMessage("Sorry, a problem occurred trying to shutdown")
+			textChannel.sendMessage(config.strings.commandShutdownError)
 				.complete()
 
 			return
 		}
 
 		if (!(authorAsGuildMember.isAllowedToShutdownBot())) {
-			textChannel.sendMessage("You are not allowed to use this command")
+			textChannel.sendMessage(config.strings.commandShutdownInsufficientPermissions)
 				.complete()
 
 			return
 		}
 
-		textChannel.sendMessage("Shutting down. Bye bye...")
+		textChannel.sendMessage(config.strings.commandShutdownResponse)
 			.complete()
 
 		// we can't actually do the shutdown procedure here, since we're still in the event listener.
--- bot/src/main/kotlin/io/github/maybeashleyidk/discordbot/compoundzebracommunity/config/Config.kt
+++ bot/src/main/kotlin/io/github/maybeashleyidk/discordbot/compoundzebracommunity/config/Config.kt
@@ -1,8 +1,18 @@
 package io.github.maybeashleyidk.discordbot.compoundzebracommunity.config
 
+import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class Config(
-	val dummy: Int?,
+	@Json(name = "strings") val strings: LanguageStrings,
+)
+
+@JsonClass(generateAdapter = true)
+data class LanguageStrings(
+	@Json(name = "generic.invalidCommandName") val genericInvalidCommandName: String,
+	@Json(name = "generic.unknownCommand") val genericUnknownCommand: String,
+	@Json(name = "command.shutdown.response") val commandShutdownResponse: String,
+	@Json(name = "command.shutdown.insufficientPermissions") val commandShutdownInsufficientPermissions: String,
+	@Json(name = "command.shutdown.error") val commandShutdownError: String,
 )
