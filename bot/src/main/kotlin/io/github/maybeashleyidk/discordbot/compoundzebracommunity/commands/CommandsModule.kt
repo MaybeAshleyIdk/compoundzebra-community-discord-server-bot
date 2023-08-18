ab62da80c593fa7fa116eb53a6a5bdf3f74bea1f
MaybeAshleyIdk <142437333+MaybeAshleyIdk@users.noreply.github.com>
2023-08-18 12:15:41 +0200

    Add configuration of echo commands

--- bot/src/main/kotlin/io/github/maybeashleyidk/discordbot/compoundzebracommunity/commands/CommandsModule.kt
+++ bot/src/main/kotlin/io/github/maybeashleyidk/discordbot/compoundzebracommunity/commands/CommandsModule.kt
@@ -3,8 +3,12 @@ package io.github.maybeashleyidk.discordbot.compoundzebracommunity.commands
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
+import dagger.multibindings.ElementsIntoSet
 import dagger.multibindings.IntoSet
 import dagger.multibindings.Multibinds
+import io.github.maybeashleyidk.discordbot.compoundzebracommunity.config.Config
+import io.github.maybeashleyidk.discordbot.compoundzebracommunity.config.ConfigLoader
+import io.github.maybeashleyidk.discordbot.compoundzebracommunity.config.EchoCommandDefinition
 
 @Module(
 	includes = [
@@ -24,13 +28,30 @@ object CommandsModule {
 		fun bindShutdownCommand(shutdownCommand: ShutdownCommand): Command
 	}
 
+	// TODO: find a way to do this dynamic
 	@Provides
-	@IntoSet
-	fun providePingCommand(echoCommandFactory: EchoCommand.Factory): Command {
-		return echoCommandFactory
-			.build(
-				name = CommandName.ofString("ping"),
-				responseMessage = "pong",
-			)
+	@ElementsIntoSet
+	fun provideConfiguredEchoCommands(
+		echoCommandFactory: EchoCommand.Factory,
+		configLoader: ConfigLoader,
+	): Set<Command> {
+		val config: Config = configLoader.load()
+		return config.echoCommandDefinitions
+			.mapKeys { (commandNameStr: String, _: EchoCommandDefinition) ->
+				CommandName.ofString(commandNameStr)
+			}
+			.mapTo(LinkedHashSet(config.echoCommandDefinitions.size)) {
+					(
+						commandName: CommandName,
+						echoCommandDefinition: EchoCommandDefinition,
+					),
+				->
+
+				echoCommandFactory
+					.build(
+						commandName,
+						echoCommandDefinition.responseMessage,
+					)
+			}
 	}
 }
--- bot/src/main/kotlin/io/github/maybeashleyidk/discordbot/compoundzebracommunity/config/Config.kt
+++ bot/src/main/kotlin/io/github/maybeashleyidk/discordbot/compoundzebracommunity/config/Config.kt
@@ -6,6 +6,7 @@ import com.squareup.moshi.JsonClass
 @JsonClass(generateAdapter = true)
 data class Config(
 	@Json(name = "strings") val strings: LanguageStrings,
+	@Json(name = "echoCommands") val echoCommandDefinitions: Map<String, EchoCommandDefinition>,
 )
 
 @JsonClass(generateAdapter = true)
@@ -15,4 +16,9 @@ data class LanguageStrings(
 	@Json(name = "command.shutdown.response") val commandShutdownResponse: String,
 	@Json(name = "command.shutdown.insufficientPermissions") val commandShutdownInsufficientPermissions: String,
 	@Json(name = "command.shutdown.error") val commandShutdownError: String,
+)
+
+@JsonClass(generateAdapter = true)
+data class EchoCommandDefinition(
+	@Json(name = "response") val responseMessage: String,
 )
