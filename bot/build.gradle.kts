f5a8ab99f810a99d415d7f2b9343d49a0f2addae
MaybeAshleyIdk <142437333+MaybeAshleyIdk@users.noreply.github.com>
2023-08-18 14:07:08 +0200

    Move common gradle logic into project build file

--- bot/build.gradle.kts
+++ bot/build.gradle.kts
@@ -9,18 +9,6 @@ plugins {
 group = "io.github.maybeashleyidk"
 version = "0.1.0-indev02"
 
-val javaCompatibilityVersion: JavaVersion = JavaVersion.VERSION_17
-
-java {
-	sourceCompatibility = javaCompatibilityVersion
-	targetCompatibility = javaCompatibilityVersion
-	toolchain.languageVersion.set(JavaLanguageVersion.of(javaCompatibilityVersion.majorVersion))
-}
-
-kotlin {
-	jvmToolchain(javaCompatibilityVersion.majorVersion.toInt())
-}
-
 application {
 	mainClass.set("io.github.maybeashleyidk.discordbot.compoundzebracommunity.Main")
 }
--- utils/build.gradle.kts
+++ utils/build.gradle.kts
@@ -3,19 +3,6 @@ plugins {
 	`java-library`
 }
 
-val javaCompatibilityVersion: JavaVersion = JavaVersion.VERSION_17
-
-java {
-	sourceCompatibility = javaCompatibilityVersion
-	targetCompatibility = javaCompatibilityVersion
-	toolchain.languageVersion.set(JavaLanguageVersion.of(javaCompatibilityVersion.majorVersion))
-}
-
-kotlin {
-	jvmToolchain(javaCompatibilityVersion.majorVersion.toInt())
-	explicitApi()
-}
-
 dependencies {
 	implementation("com.google.code.findbugs:jsr305:3.0.2")
 }
