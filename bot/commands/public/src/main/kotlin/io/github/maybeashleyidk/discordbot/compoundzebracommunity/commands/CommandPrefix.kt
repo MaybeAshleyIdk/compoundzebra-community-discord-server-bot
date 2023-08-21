bc542adaed57ec9e3c2f3af09c8c980e090f2bde
MaybeAshleyIdk <142437333+MaybeAshleyIdk@users.noreply.github.com>
2023-08-21 22:53:07 +0200

    Add more restrictions to the command prefix

--- bot/commands/public/src/main/kotlin/io/github/maybeashleyidk/discordbot/compoundzebracommunity/commands/CommandPrefix.kt
+++ bot/commands/public/src/main/kotlin/io/github/maybeashleyidk/discordbot/compoundzebracommunity/commands/CommandPrefix.kt
@@ -1,5 +1,6 @@
 package io.github.maybeashleyidk.discordbot.compoundzebracommunity.commands
 
+import io.github.maybeashleyidk.discordbot.compoundzebracommunity.utils.quoted
 import javax.annotation.CheckReturnValue
 
 @JvmInline
@@ -7,10 +8,25 @@ public value class CommandPrefix private constructor(public val string: String) 
 
 	public companion object {
 
+		private const val VALID_PREFIX_CHARS: String = "!#%&()+,-.:;=>?[]^{|}~"
+
+		@CheckReturnValue
+		private fun isValidCommandPrefixChar(ch: Char): Boolean {
+			return this.VALID_PREFIX_CHARS
+				.any { validPrefixChar: Char ->
+					validPrefixChar == ch
+				}
+		}
+
+		@CheckReturnValue
+		public fun isValidCommandPrefix(prefixString: String): Boolean {
+			return prefixString.isNotEmpty() && prefixString.all(this::isValidCommandPrefixChar)
+		}
+
 		@CheckReturnValue
 		public fun ofString(prefixString: String): CommandPrefix {
-			require(prefixString.isNotBlank()) {
-				"Prefix string must not be blank"
+			require(this.isValidCommandPrefix(prefixString)) {
+				"Invalid prefix string ${prefixString.quoted()}"
 			}
 
 			return CommandPrefix(prefixString)
