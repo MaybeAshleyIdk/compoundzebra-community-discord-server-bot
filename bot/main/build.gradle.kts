a7ab3ba4ef40597745c610596a92ce58cb82673f
MaybeAshleyIdk <142437333+MaybeAshleyIdk@users.noreply.github.com>
2023-09-23 18:35:04 +0200

    Fix the module :bot:main

--- bot/main/build.gradle.kts
+++ bot/main/build.gradle.kts
@@ -8,9 +8,11 @@ dependencies {
 	implementation(project(":bot:bot-wiring"))
 
 	implementation(project(":bot:logging:bot-logging-public"))
-	implementation(project(":bot:features:shutdown:bot-features-shutdown-public"))
+	implementation(project(":bot:features:shutdown:bot-features-shutdown-impl"))
 
 	implementation(libs.jda) {
 		exclude(module = "opus-java")
 	}
+
+	implementation(libs.dagger)
 }
