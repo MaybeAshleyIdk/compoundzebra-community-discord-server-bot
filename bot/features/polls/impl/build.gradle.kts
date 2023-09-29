8a00433d71c7b8de6cdd238a3f9af3355ee54c63
MaybeAshleyIdk <142437333+MaybeAshleyIdk@users.noreply.github.com>
2023-09-29 18:18:16 +0200

    Use coroutines in the class `PollEventsListener`

--- bot/features/polls/impl/build.gradle.kts
+++ bot/features/polls/impl/build.gradle.kts
@@ -9,6 +9,9 @@ dependencies {
 	implementation(project(":bot:config:supplier:bot-config-supplier-public"))
 	implementation(project(":bot:bot-snowflake"))
 	implementation(project(":bot:logging:bot-logging-public"))
+	implementation(project(":bot:utils:coroutines:bot-utils-coroutines-jda"))
+
+	implementation(libs.kotlinx.coroutines.core)
 
 	implementation(libs.jda) {
 		exclude(module = "opus-java")
