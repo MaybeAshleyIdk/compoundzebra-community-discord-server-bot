e462d8ad90fd95d0d6a31ad29bd100a7327ccd9d
MaybeAshleyIdk <142437333+MaybeAshleyIdk@users.noreply.github.com>
2023-10-31 00:15:59 +0100

    Log conditional messages

--- bot/conditional-message-event-handling/impl/build.gradle.kts
+++ bot/conditional-message-event-handling/impl/build.gradle.kts
@@ -7,7 +7,9 @@ plugins {
 dependencies {
 	api(project(":bot:conditional-message-event-handling:api"))
 	api(project(":bot:config-supplier"))
+	api(project(":bot:logging"))
 
+	implementation(project(":bot:utils"))
 	implementation(project(":bot:utils-coroutines-jda"))
 
 	implementation(libs.kotlinx.coroutines.core)
