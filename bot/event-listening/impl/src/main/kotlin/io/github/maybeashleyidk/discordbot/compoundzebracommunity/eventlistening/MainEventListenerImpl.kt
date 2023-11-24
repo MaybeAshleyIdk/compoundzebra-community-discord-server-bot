86476099041de2ac5ab70bd56010eed5266315eb
MaybeAshleyIdk <142437333+MaybeAshleyIdk@users.noreply.github.com>
2023-11-24 14:44:56 +0100

    Use thread pools to dispatch events to handlers

--- bot/event-listening/impl/src/main/kotlin/io/github/maybeashleyidk/discordbot/compoundzebracommunity/eventlistening/MainEventListenerImpl.kt
+++ bot/event-listening/impl/src/main/kotlin/io/github/maybeashleyidk/discordbot/compoundzebracommunity/eventlistening/MainEventListenerImpl.kt
@@ -5,17 +5,38 @@ import io.github.maybeashleyidk.discordbot.compoundzebracommunity.logging.Logger
 import io.github.maybeashleyidk.discordbot.compoundzebracommunity.messageeventhandlermediation.MessageEventHandlerMediator
 import io.github.maybeashleyidk.discordbot.compoundzebracommunity.polleventlistening.PollEventListener
 import io.github.maybeashleyidk.discordbot.compoundzebracommunity.privatemessageeventhandling.PrivateMessageEventHandler
+import io.github.maybeashleyidk.discordbot.compoundzebracommunity.shutdowncallbackregistry.ShutdownCallbackRegistry
+import io.github.maybeashleyidk.discordbot.compoundzebracommunity.shutdowncallbacks.OnAfterShutdownCallback
+import io.github.maybeashleyidk.discordbot.compoundzebracommunity.shutdowncallbacks.OnBeforeShutdownCallback
 import io.github.maybeashleyidk.discordbot.compoundzebracommunity.shutdowneventhandler.ShutdownEventHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.joinAll
+import kotlinx.coroutines.launch
 import net.dv8tion.jda.api.events.GenericEvent
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.cancellation.CancellationException
 
 public class MainEventListenerImpl @Inject constructor(
 	private val logger: Logger,
 	private val shutdownEventHandler: ShutdownEventHandler,
+	shutdownCallbackRegistry: ShutdownCallbackRegistry,
 	messageEventHandlerMediator: MessageEventHandlerMediator,
 	pollEventListener: PollEventListener,
 	privateMessageEventHandler: PrivateMessageEventHandler,
 ) : MainEventListener {
+
+	private companion object {
+
+		const val INCOMING_EVENTS_THREAD_POOL_TERMINATION_TIMEOUT_MS: Long = 2500
+
+		const val EVENT_HANDLERS_THREAD_POOL_TERMINATION_TIMEOUT_MS: Long = 7500
+	}
 
 	private val otherEventHandlers: Set<EventHandler> =
 		setOf(
@@ -24,23 +45,93 @@ public class MainEventListenerImpl @Inject constructor(
 			privateMessageEventHandler,
 		)
 
+	@Volatile
+	private var isShuttingDownOrIsShutDown: Boolean = false
+
+	private val incomingEventsThreadPool: ExecutorService = Executors.newCachedThreadPool()
+	private val incomingEventsCoroutineContext: CoroutineContext = this.incomingEventsThreadPool.asCoroutineDispatcher()
+
+	private val eventHandlersThreadPool: ExecutorService = Executors.newCachedThreadPool()
+	private val eventHandlersCoroutineContext: CoroutineContext = this.eventHandlersThreadPool.asCoroutineDispatcher()
+
+	init {
+		val onBeforeShutdownCallback =
+			OnBeforeShutdownCallback {
+				this.isShuttingDownOrIsShutDown = true
+			}
+
+		val onAfterShutdownCallback = OnAfterShutdownCallback(this::shutdownThreadPools)
+
+		shutdownCallbackRegistry.registerCallback(onBeforeShutdownCallback)
+		shutdownCallbackRegistry.registerCallback(onAfterShutdownCallback)
+	}
+
 	override fun onEvent(event: GenericEvent) {
+		if (this.isShuttingDownOrIsShutDown) {
+			return
+		}
+
+		// each incoming event gets its own coroutine scope
+		CoroutineScope(this.incomingEventsCoroutineContext)
+			.launch {
+				this@MainEventListenerImpl.handleEvent(event)
+			}
+	}
+
+	private suspend fun handleEvent(event: GenericEvent) {
 		val status: ShutdownEventHandler.Status = this.shutdownEventHandler.handleEvent(event)
 		when (status) {
 			ShutdownEventHandler.Status.STILL_RUNNING -> Unit
 			ShutdownEventHandler.Status.SHUTTING_DOWN -> return
 		}
 
-		for (eventHandler: EventHandler in this.otherEventHandlers) {
-			try {
-				eventHandler.handleEvent(event)
-			} catch (e: Throwable) {
-				this.logger.logError(e, "Event handler $eventHandler threw an exception")
+		val eventHandlingJobs: List<Job> = this.otherEventHandlers
+			.map { eventHandler: EventHandler ->
+				// each event handler get its own coroutine scope
+				CoroutineScope(this.eventHandlersCoroutineContext)
+					.launch {
+						this@MainEventListenerImpl.executeEventHandler(eventHandler, event)
+					}
+			}
 
-				if (e !is Exception) {
-					throw e
-				}
+		eventHandlingJobs.joinAll()
+	}
+
+	private suspend fun executeEventHandler(eventHandler: EventHandler, event: GenericEvent) {
+		try {
+			eventHandler.handleEvent(event)
+		} catch (e: Throwable) {
+			if (e is CancellationException) {
+				throw e
+			}
+
+			this.logger.logError(e, "Event handler $eventHandler threw an exception")
+
+			if (e !is Exception) {
+				throw e
 			}
 		}
+	}
+
+	private fun shutdownThreadPools() {
+		this.incomingEventsThreadPool.shutdown()
+		this.eventHandlersThreadPool.shutdown()
+
+		@Suppress("RemoveRedundantQualifierName")
+		val incomingEventsThreadPoolTerminated: Boolean = this.incomingEventsThreadPool
+			.awaitTermination(
+				MainEventListenerImpl.INCOMING_EVENTS_THREAD_POOL_TERMINATION_TIMEOUT_MS,
+				TimeUnit.MILLISECONDS,
+			)
+
+		@Suppress("RemoveRedundantQualifierName")
+		val eventHandlersThreadPoolTerminated: Boolean =
+			this.eventHandlersThreadPool.awaitTermination(
+				MainEventListenerImpl.EVENT_HANDLERS_THREAD_POOL_TERMINATION_TIMEOUT_MS,
+				TimeUnit.MILLISECONDS,
+			)
+
+		if (!incomingEventsThreadPoolTerminated) this.incomingEventsThreadPool.shutdownNow()
+		if (!eventHandlersThreadPoolTerminated) this.eventHandlersThreadPool.shutdownNow()
 	}
 }
