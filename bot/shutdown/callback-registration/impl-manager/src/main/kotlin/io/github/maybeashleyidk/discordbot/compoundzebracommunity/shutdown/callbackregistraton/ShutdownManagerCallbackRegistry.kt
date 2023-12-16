724875e2a80c1b9b0aa23a2feabca448540cd964
MaybeAshleyIdk <142437333+MaybeAshleyIdk@users.noreply.github.com>
2023-12-16 19:10:09 +0100

    Match shutdown packages to the module names

---
+++ bot/shutdown/callback-registration/impl-manager/src/main/kotlin/io/github/maybeashleyidk/discordbot/compoundzebracommunity/shutdown/callbackregistraton/ShutdownManagerCallbackRegistry.kt
@@ -0,0 +1,27 @@
+package io.github.maybeashleyidk.discordbot.compoundzebracommunity.shutdown.callbackregistraton
+
+import io.github.maybeashleyidk.discordbot.compoundzebracommunity.shutdown.callbacks.OnAfterShutdownCallback
+import io.github.maybeashleyidk.discordbot.compoundzebracommunity.shutdown.callbacks.OnBeforeShutdownCallback
+import io.github.maybeashleyidk.discordbot.compoundzebracommunity.shutdown.management.ShutdownManager
+import javax.inject.Inject
+
+public class ShutdownManagerCallbackRegistry @Inject constructor(
+	private val shutdownManager: ShutdownManager,
+) : ShutdownCallbackRegistry {
+
+	override fun registerCallback(callback: OnBeforeShutdownCallback) {
+		this.shutdownManager.registerShutdownCallback(callback)
+	}
+
+	override fun registerCallback(callback: OnAfterShutdownCallback) {
+		this.shutdownManager.registerShutdownCallback(callback)
+	}
+
+	override fun unregisterCallback(callback: OnAfterShutdownCallback) {
+		this.shutdownManager.unregisterShutdownCallback(callback)
+	}
+
+	override fun unregisterCallback(callback: OnBeforeShutdownCallback) {
+		this.shutdownManager.unregisterShutdownCallback(callback)
+	}
+}
---
+++ bot/shutdown/event-handling/impl-manager/src/main/kotlin/io/github/maybeashleyidk/discordbot/compoundzebracommunity/shutdown/eventhandling/ShutdownManagerEventHandler.kt
@@ -0,0 +1,17 @@
+package io.github.maybeashleyidk.discordbot.compoundzebracommunity.shutdown.eventhandling
+
+import io.github.maybeashleyidk.discordbot.compoundzebracommunity.shutdown.management.ShutdownManager
+import net.dv8tion.jda.api.events.GenericEvent
+import javax.inject.Inject
+
+public class ShutdownManagerEventHandler @Inject constructor(
+	private val shutdownManager: ShutdownManager,
+) : ShutdownEventHandler {
+
+	override suspend fun handleEvent(event: GenericEvent): ShutdownEventHandler.Status {
+		return when (this.shutdownManager.handleShutdownEvent(event)) {
+			ShutdownManager.EventHandlingResultStatus.STILL_RUNNING -> ShutdownEventHandler.Status.STILL_RUNNING
+			ShutdownManager.EventHandlingResultStatus.SHUTTING_DOWN -> ShutdownEventHandler.Status.SHUTTING_DOWN
+		}
+	}
+}
