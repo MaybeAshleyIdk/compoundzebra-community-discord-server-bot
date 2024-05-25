b03102a654eab4fb9eb7e157d5fe8bfa38c1bb3f
MaybeAshleyIdk <142437333+MaybeAshleyIdk@users.noreply.github.com>
2024-05-25 17:59:50 +0200

    Apply the convenience plugin to all projects
    Note that this commit also removes the JVM toolchains.
    
    See the following blogpost by Jake Wharton as to why:
    <https://jakewharton.com/gradle-toolchains-are-rarely-a-good-idea/>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
-import org.jetbrains.kotlin.gradle.dsl.KotlinTopLevelExtension
-import org.jetbrains.kotlin.gradle.plugin.KotlinPluginWrapper
+import io.github.maybeashleyidk.discordbot.compoundzebracommunity.build.ConveniencePlugin
 
 plugins {
 	// The line
@@ -14,51 +13,6 @@ plugins {
 	alias(libs.plugins.shadow) apply false
 }
 
-val javaCompatibilityVersion: JavaVersion = JavaVersion.VERSION_17
-
-subprojects {
-	plugins.withType<ApplicationPlugin> {
-		configureJavaExtension(project = this@subprojects)
-
-		plugins.withType<KotlinPluginWrapper> {
-			extensions.configure<KotlinTopLevelExtension> {
-				applyCommonKotlinConfigurations()
-			}
-		}
-	}
-
-	plugins.withType<JavaLibraryPlugin> {
-		configureJavaExtension(project = this@subprojects)
-
-		plugins.withType<KotlinPluginWrapper> {
-			extensions.configure<KotlinTopLevelExtension> {
-				applyCommonKotlinConfigurations()
-				explicitApi()
-			}
-		}
-	}
-
-	plugins.withType<KotlinPluginWrapper> {
-		check(plugins.hasPlugin<ApplicationPlugin>() || plugins.hasPlugin<JavaLibraryPlugin>()) {
-			"Before the Kotlin plugin is applied, either the Java application or Java library plugin must tbe applied"
-		}
-	}
-}
-
-fun configureJavaExtension(project: Project) {
-	project.extensions.configure<JavaPluginExtension> {
-		sourceCompatibility = javaCompatibilityVersion
-		targetCompatibility = javaCompatibilityVersion
-		toolchain {
-			languageVersion.set(JavaLanguageVersion.of(javaCompatibilityVersion.majorVersion))
-		}
-	}
-}
-
-fun KotlinTopLevelExtension.applyCommonKotlinConfigurations() {
-	jvmToolchain(javaCompatibilityVersion.majorVersion.toInt())
-}
-
-inline fun <reified T : Plugin<*>> PluginContainer.hasPlugin(): Boolean {
-	return this@hasPlugin.hasPlugin(T::class)
+allprojects {
+	apply<ConveniencePlugin>()
 }
